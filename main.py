addd15c635c28c3d737ee3e0eb0c648aa87b5ad9
Jack Aitken <jaitk11@gmail.com>
2021-01-03 00:42:47 -0500

    includes gameplay but bug only shows 48 cards.

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from random import randint
+import keyboard
+
 suit = ["Hearts", "Diamonds", "Clubs", "Spades"]
 color = ["Red", "Black"]
 face = ["Jack", "Queen", "King", "Ace"]
@@ -12,26 +15,69 @@ class Card:
 class Deck:
 	def __init__(self):
 		self.cards = []
+		self.cards_remaining = 51
+
+	def no_cards_left(self):
+		return len(self.cards) == 0
+
+	def card_dealt(self):
+		self.cards_remaining -= 1
+
+def main(deck):
+	# deal red
+	for i in range(0, 2):
+		for j in range(2, 11):
+			card = Card(suit[i], color[0], j)
+			deck.cards.append(card)
+			
+		for k in range(3):
+			card = Card(suit[i], color[1], None, face[k])
+			deck.cards.append(card)
+
+	# deal black
+	for i in range(2, 4):
+		for j in range(2, 11):
+			card = Card(suit[i], color[1], j)
+			deck.cards.append(card)
+		
+		for k in range(3):
+			card = Card(suit[i], color[1], None, face[k])
+			deck.cards.append(card)
+
+	user_count = 0
+	for i in range(len(deck.cards)):
+		card = deck.cards[randint(0, deck.cards_remaining)]
+		deck.cards.remove(card)
+		deck.card_dealt()
+		if card.face:
+			user_count += 10
+			print(f"{card.face} of {card.suit}")
+			if user_count > 21:
+				print(f"Oh no! {user_count}. You bust!")
+				break
+			elif user_count == 21:
+				print("21! You win!")
+
+		else:
+			user_count += card.number
+			print(f"{card.number} of {card.suit}")
+			if user_count > 21:
+				print(f"Oh no! {user_count}. You bust!")
+				break
+			elif user_count == 21:
+				print("21! You win!")
+
+		print(f"Type \"h\" to hit or \"s\" to stay", end="\n")
+
+		user_input = input()
+
+		if user_input == "h":
+			continue
+
+		else:
+			print("Thanks for playing!")
+			break
+
 
 deck = Deck()
-
-# deal red
-for i in range(0, 2):
-	for j in range(1, 11):
-		card = Card(suit[i], color[0], j)
-		deck.cards.append((card.color, card.number, card.suit))
-		
-	for k in range(3):
-		card = Card(suit[i], color[1], face[k])
-		deck.cards.append((card.color, card.number, card.suit))
-
-# deal black
-for i in range(2, 4):
-	for j in range(1, 11):
-		card = Card(suit[i], color[1], j)
-		deck.cards.append((card.color, card.number, card.suit))
-	
-	for k in range(3):
-		card = Card(suit[i], color[1], face[k])
-		deck.cards.append((card.color, card.number, card.suit))
-
+main(deck)
